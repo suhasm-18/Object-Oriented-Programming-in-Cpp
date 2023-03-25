d394e0d39214bc27e973c4e9f6e111b7c2579400
Suhas.m.malagihal <115938641+suhasm-18@users.noreply.github.com>
2023-03-25 22:38:41 +0530

    Create Inheritanceconstructor.cpp

---
+++ Inheritanceconstructor.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+class Base
+{
+public:
+ Base(){cout<<"Non-param Base"<<endl;}
+ Base(int x){cout<<"Param of Base "<<x<<endl;}
+};
+class Derived:public Base
+{
+public:
+ Derived(){cout<<"Non-Param Derived"<<endl;}
+ Derived(int y){cout<<"Param of Derived "<<y<<endl;}
+ 
+ Derived(int x,int y):Base(x)
+ {
+ cout<<"Param of Derived "<<y<<endl;
+ 
+ }
+};
+int main()
+{
+ Derived d(5,10);
+}
