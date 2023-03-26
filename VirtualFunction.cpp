1065eb271c15925ca12fbde3b4e1d7bcd090292a
Suhas.m.malagihal <115938641+suhasm-18@users.noreply.github.com>
2023-03-26 10:33:02 +0530

    Create VirtualFunction.cpp

---
+++ VirtualFunction.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+class BasicCar
+{
+public:
+ virtual void start(){cout<<"BasicCar started"<<endl;}
+};
+class AdvanceCar: public BasicCar
+{
+public:
+ void start(){cout<<"AdvanceCar Started"<<endl;}
+};
+int main()
+{
+ BasicCar *p=new AdvanceCar();
+ p->start();
+}
