c47fc8b27eb0492e00fbb47c5ee3fb25a608adc0
Suhas.m.malagihal <115938641+suhasm-18@users.noreply.github.com>
2023-04-12 20:21:13 +0530

    Update DSAOOPS.cpp

--- DSAOOPS.cpp
+++ DSAOOPS.cpp
@@ -26,5 +26,39 @@ int main()
 	return 0;
 }
 
+golf::golf(const char * name , int hc)
+{
+	strcpy(fullname, name);
+	handicap = hc;
+}
 
+golf::~golf()
+{
+}
+
+int golf::setgolf(){
+	
+	int wczytanie;
+	int temp_hendi;
+	char imie[40];
+	cout << "Podaj imie: ";
+	cin.getline(imie, 40);
+	if(strlen(imie))
+	{
+		wczytanie = 1;
+	}
+	else
+		wczytanie = 0;
+	cout  << "Podaj handicap: ";
+	cin >> temp_hendi;
+	golf tymczasowy(imie, temp_hendi);
+	*this = tymczasowy;
+	return wczytanie;
+}
+
+void golf::showgolf() const
+{
+	 cout << "\nHandicup: " << this->handicap << endl;
+	 cout << "\nImie: " << this->fullname << endl;
+}
 
