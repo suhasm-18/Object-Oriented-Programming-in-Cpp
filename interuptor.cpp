76ca33eb8f86a6746fa0e52b32ac2a84a2b413c8
Suhas.m.malagihal <115938641+suhasm-18@users.noreply.github.com>
2023-04-13 18:57:41 +0530

    Create interuptor.cpp

---
+++ interuptor.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a,b;
+    cout<<"enter two variables";
+    cin>> a >>b;
+    try
+    {
+        if( b!=0){
+            cout<<"res :"<< a / b <<endl;
+        }
