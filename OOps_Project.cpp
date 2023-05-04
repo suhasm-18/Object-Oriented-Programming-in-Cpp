cd1ee748108ac3b7f57bc53cb1bd934b918aea49
Suhas.m.malagihal <115938641+suhasm-18@users.noreply.github.com>
2023-05-04 23:54:46 +0530

    Update OOps_Project.cpp

--- OOps_Project.cpp
+++ OOps_Project.cpp
@@ -16,4 +16,19 @@ class Employee{
         Company = company;
         Age = age;
     }
+};
 
+int main(){
+    Employee employee1 = Employee("Suhas" , "CEO" , 55);
+    employee1.Intro();
+
+    Employee employee2 = Employee("jhon" , "MD" , 85);
+    employee2.Intro();
+    // Employee employee2;
+    // employee2.Name = "ram";
+    // employee2.Company = "TATA";
+    // employee2.Age= 88;
+    // employee2.Intro()
+    
+
+    }
